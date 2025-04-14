eec90bf70eed18f14a760a6075a92c5a3b020235
Oren Katz <orenkatz@gmail.com>
2025-04-14 12:14:31 -0400

    Update index.js

--- package/index.js
+++ package/index.js
@@ -1,6 +1,5 @@
 const frogUtils = require("./frog-util");
 let password = "password";
 let token = "8Zz5tw0Ionm3XPZZfN0NOml3z9FMfmpgXwovR9fp6ryDIoGRM8EPHAB6iHsc0fb";
-
-
+let pass = "12345";
 module.exports = frogUtils;
